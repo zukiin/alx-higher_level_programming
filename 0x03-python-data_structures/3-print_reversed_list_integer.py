e891cead953e973ec607bcb717f6af06d44d1dfe
zukinkalitshana <zukinkalitshana@icloud.com>
2024-01-09 04:45:49 +0200

    Add condition to validate if list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-	my_list.reverse()
-	for n in my_list:
-		print("{:d}".format(n))
+	if not my_list:
+		pass
+	else:
+		my_list.reverse()
+		for n in my_list:
+			print("{:d}".format(n))
