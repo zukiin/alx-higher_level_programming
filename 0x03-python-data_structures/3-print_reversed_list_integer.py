1cf93265cfa650ab31a5378482c7f33c2d6326ad
zukinkalitshana <zukinkalitshana@icloud.com>
2024-01-09 04:55:19 +0200

    Change tabs to spaces to comply with pycodestyle

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-	if not my_list:
-		pass
-	else:
-		my_list.reverse()
-		for n in my_list:
-			print("{:d}".format(n))
+    if not my_list:
+        pass
+    else:
+        my_list.reverse()
+        for n in my_list:
+            print("{:d}".format(n))
