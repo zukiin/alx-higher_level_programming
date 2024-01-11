8e05101edf3b5b5c656109380a44efdc505f23df
zukinkalitshana <zukinkalitshana@icloud.com>
2024-01-11 04:36:21 +0200

    Change implementation to use map

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,12 +3,4 @@
 def square_matrix_simple(matrix=[]):
     if not matrix:
         return None
-
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for e in row:
-            new_row.append(e ** 2)
-        new_matrix.append(new_row)
-
-    return new_matrix
+    return list(list(map(lambda x: x*x, new_list)) for new_list in matrix)
