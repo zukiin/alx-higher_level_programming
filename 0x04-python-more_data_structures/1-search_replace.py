c22a2b4b1de31fcec515a1a84dea9835a031327c
zukinkalitshana <zukinkalitshana@icloud.com>
2024-01-11 04:30:56 +0200

    Fix indentation

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,10 +5,10 @@ def search_replace(my_list, search, replace):
         return my_list
 
     new_list = []
-	for val in my_list:
+    for val in my_list:
         if val != search:
-        new_list.append(val)
-    else:
-        new_list.append(replace)
+            new_list.append(val)
+        else:
+            new_list.append(replace)
 
     return new_list
