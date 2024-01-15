73644a877c6cfcfae1586fc6b20191c596fe6cb2
zukinkalitshana <zukinkalitshana@icloud.com>
2024-01-15 18:26:41 +0200

    Add script that prints if a number meets any of the specified conditions

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+the_str = "Last digit of"
+num_as_str = str(number)
+last_num = int(num_as_str[-1])
+
+if last_num > 5:
+    print(f"{the_str} {number} is {last_num} and is greater than 5")
+elif last_num == 0:
+    print(f"{the_str} {number} is {last_num} and is 0")
+elif last_num > 0 and last_num < 6:
+     print(f"{the_str} {number} is {last_num} and is less than 6 and not 0")
