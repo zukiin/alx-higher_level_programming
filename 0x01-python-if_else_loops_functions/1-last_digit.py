56bb8519bcaa4d4e56df09011a3d6bf5a409e0fa
zukinkalitshana <zukinkalitshana@icloud.com>
2024-01-15 18:38:22 +0200

    Fix code to follow the pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,12 +6,12 @@ the_str = "Last digit of"
 num_as_str = str(number)
 last_num = int(num_as_str[-1])
 
-if number < 0:                                                                  
-     last_num = -last_num 
+if number < 0:
+    last_num = -last_num
 
 if last_num > 5:
     print(f"{the_str} {number} is {last_num} and is greater than 5")
 elif last_num == 0:
     print(f"{the_str} {number} is {last_num} and is 0")
 elif last_num < 6 and last_num != 0:
-     print(f"{the_str} {number} is {last_num} and is less than 6 and not 0")
+    print(f"{the_str} {number} is {last_num} and is less than 6 and not 0")
