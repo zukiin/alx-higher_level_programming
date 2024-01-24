49fa281ccd64a39081c237f51eb8ee19b979eaac
zukinkalitshana <zukinkalitshana@icloud.com>
2024-01-24 08:32:52 +0200

    Remove trailing whitespace

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            counter += 1       
+            counter += 1
         except (TypeError, ValueError):
             pass
 
