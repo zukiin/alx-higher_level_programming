6ae5d9e1ba797feeadb5914583fd601a07618fa9
zukinkalitshana <zukinkalitshana@icloud.com>
2024-01-25 23:06:17 +0200

    Add FizzBuzz program

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    n = 1
+    str_1 = "Fizz"
+    str_2 = "Buzz"
+    str_3 = "FizzBuzz"
+
+    while(n < 101):
+        if (n % 3 == 0):
+            print("{}".format(str_1), end=" ")
+        elif (n % 5 == 0):
+            print("{}".format(str_2), end=" ")
+        elif (n % 3 == 0 and n % 5 == 0):
+            print("{}".format(str_3), end=" ")
+        else:
+            print("{:d}".format(n), end=" ")
+
+        n += 1
